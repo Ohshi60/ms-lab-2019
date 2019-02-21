32379fe9d6b38a2dcbef939a1d842fab460d2884
Michael S <lakseborgen@gmail.com>
2019-02-21 14:35:30 +0100

    add structs

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+// JSON object
+{
+    "seed": 1234,
+    "width": 14,
+    "height": 14,
+    "entry": 1,
+    "exit": 196,
+    "cells": [[{"north": true, "south": true, "east": false, "west": false}, ...], ...]
+}
+ 
+// C data structures
+ 
+struct cell {
+    boolean north;
+    boolean south;
+    boolean east;
+    boolean west;
+}
+ 
+struct labyrinth {
+    int seed;
+    int width;
+    int height;
+    int entry;
+    int exit;
+    struct cell **cells;
+}
+
 int main()
 {
 	printf("Hello, world");
