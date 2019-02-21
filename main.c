b4dfbedb7d73e0afb7a1c9fed1b609eaaac61bcd
Michael S <lakseborgen@gmail.com>
2019-02-21 21:41:29 +0100

    add testfile working on loader

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 // JSON object
-{
+/*{
     "seed": 1234,
     "width": 14,
     "height": 14,
@@ -9,15 +11,19 @@
     "exit": 196,
     "cells": [[{"north": true, "south": true, "east": false, "west": false}, ...], ...]
 }
- 
+ */
 // C data structures
- 
+
+//Prototypes
+int loadLab();
+//Data Structures
+
 struct cell {
-    boolean north;
-    boolean south;
-    boolean east;
-    boolean west;
-}
+    int north;
+    int south;
+    int east;
+    int west;
+};
  
 struct labyrinth {
     int seed;
@@ -26,12 +32,44 @@ struct labyrinth {
     int entry;
     int exit;
     struct cell **cells;
-}
+} laby;
 
 int main()
 {
-	printf("Hello, world");
+	printf("***Loading Labyrinth ***\n" );
+	char *buffy;
+	loadLab();
+
 	return 0;
+}
+
+int loadLab(){
+	
+	char *buf = malloc(256);
+	char *tmp;
+	const char delim[2] = "\n";
+
+	FILE* lab = fopen("3x3_1.dat", "r");
+	if( lab == NULL ){
+		puts("Error loading file");
+		return (-1);
+	}
+	if(fgets(buf,255,lab) != NULL ){
+		puts(buf);		
+		tmp = strtok(buf, delim);
+			printf(" %s\n", tmp);	
+		/*
+		while(tmp != NULL ){
+
+			tmp = strtok(NULL,delim);
+		}	
+		*/			
+		//laby.seed = atoi(tmp);
+		return 0;
+
+	}
+	fclose(lab);
+
 }
 
 
